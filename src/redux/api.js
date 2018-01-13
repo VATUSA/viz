44037d97b4ada20ee17a008c9be28ee195309d81
Daniel Hawton <daniel@hawton.com>
2018-01-12 20:56:40 -0900

    fix production url again

--- src/redux/api.js
+++ src/redux/api.js
@@ -1,7 +1,7 @@
 // Placeholder, add dev url
 const getUrl = () => {
   if (process.env.NODE_ENV === 'production') {
-    return 'https://server.vatusa.net:8081';
+    return 'https://dock.vatusa.net:8081';
   }
   return 'http://server.vatusa.net:5201';
 };
